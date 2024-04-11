a81cf810e90bfd1c827f10b77a2bb4757f813f25
leonard <lionard.cloud@gmail.com>
2024-04-10 20:51:43 -0500

    Initial commit - context load OK

---
+++ src/main/java/com/zara/catalog/api/ZaraCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.zara.catalog.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaraCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZaraCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zara/catalog/api/ZaraCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zara.catalog.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZaraCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
